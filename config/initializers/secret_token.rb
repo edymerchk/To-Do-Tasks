edcdbcabb1ad5c0f08931185bed711709ef8b27f
edymerchk <edyhack@gmail.com>
2013-02-15 18:10:28 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TodoLinden::Application.config.secret_token = '63d09cc59a62b2698e9c408a8daaf13a4126b8090c455b6067c95538a32198d6edb430d868043a369382d64b6db79de32fe31446c89872cb718edf7074189413'
